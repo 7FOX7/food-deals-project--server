40789fd2a628184af40fdc94697f6b16cac842a5
7FOX7 <kheeugene@gmail.com>
2025-06-18 15:18:06 -0400

    Added a logic for getting products for Charlie Asian Grocery Inc. stores

--- index.ts
+++ index.ts
@@ -58,13 +58,7 @@ category: ProductCategories.NoCategory
     "Thai Binh Asian Food": () => [],       // no data for this store 
     "The Red Barn Farm Market and Bakery": () => [],  
     "Independent Grocery store - Phil's": () => import("./scraping/Independent Grocery/data").then(mod => mod.default()),
-    "Charlie Asian Grocery Inc.": () => [{
-        imageUri: "hello from product data (server)", 
-        title: "hello from product data (server)", 
-        units: "hello from product data (server)", 
-        primaryPrice: "4", 
-category: ProductCategories.NoCategory
-    }],  
+    "Charlie Asian Grocery Inc.": () => [],  // no data for this store 
     "Patty's Delights Peruvian Culinary": () => [{
         imageUri: "hello from product data (server)", 
         title: "hello from product data (server)", 
