ad5e4ed014458b60faf9703da92ab1e5452dff50
7FOX7 <kheeugene@gmail.com>
2025-06-19 20:52:38 -0400

    Added a logic for getting products for The New Food Box stores

--- index.ts
+++ index.ts
@@ -72,13 +72,7 @@ category: ProductCategories.NoCategory
     "Costco Wholesale": () => import("./scraping/Costco Wholesale/data").then(mod => mod.default()),
     "Brandon's No Frills": () => import("./scraping/No Frills/data").then(mod => mod.default()),
     "Pupo's Food Market": () => [],     // no data for this store  
-    "The New Food Box": () => [{
-        imageUri: "hello from product data (server)", 
-        title: "hello from product data (server)", 
-        units: "hello from product data (server)", 
-        primaryPrice: "4", 
-category: ProductCategories.NoCategory
-    }],  
+    "The New Food Box": () => import("./scraping/New Food Box/data").then(mod => mod.default()),
     "Kim's Variety": () => [{
         imageUri: "hello from product data (server)", 
         title: "hello from product data (server)", 
@@ -219,7 +213,7 @@ function updateProducts() {
 async function testProducts() {
     console.log("will promise to execute!")
     try {
-        const products = await productData["Costco Wholesale"]()
+        const products = await productData["The New Food Box"]()
         console.log("products: " + JSON.stringify(products))
         console.log("products length: " + products.length)
     }
---
+++ scraping/New Food Box/data.ts
@@ -0,0 +1,166 @@
+import puppeteer from "puppeteer"
+import { Products, Product } from "../../utils/types" 
+import { getProductCategory__funcBody, unitsRegex } from "../../utils/regexes"
+import * as constants from "../../utils/constants"
+
+// default for unavailable product data
+enum Unavailable {
+    ImageUri = "[COULD NOT GET PRODUCT IMAGE]", 
+    Title = "[COULD NOT GET PRODUCT NAME]", 
+    PrimaryPrice = "[COULD NOT GET PRODUCT PRIMARY PRICE]", 
+    Units = "[COULD NOT GET PRODUCT UNIT DETAILS]"
+}
+
+// we will be storing food categories: 
+// NOTE: food categories MUST match 'FoodTypes' type from our client app
+enum ProductCategories {
+    FreshProduce = "Fresh Produce",
+    MeatAndSeafood = "Meat & Seafood", 
+    DairyAndEggs = "Dairy & Eggs", 
+    FrozenAndPrepared = "Frozen & Prepared Foods", 
+    BakeryAndBreakfast = "Bakery & Breakfast", 
+    SnacksAndSweets = "Snacks & Sweets", 
+    PantryAndEssentials = "Pantry & Essentials", 
+
+    // this category will be used as a placeholder for the product 
+    // whose food category does not match any of the above (DON'T USE IT IN YOUR client app)
+    NoCategory = "[NO CATEGORY]"
+}
+
+const getData = async (): Promise<Products> => {
+    // will be storing products we will be pushing to the array: 
+    const products: Products = []
+    // will be storing the number of retries we made 
+    // to connect to the page
+    let numOfRetries = 0
+    // will be representing a browser: 
+    const browser = await puppeteer.launch({
+        headless: false
+    })
+    // NOTE: 
+    // - this value will be used to determine if we should look for the `next` button at all
+    // - if the number of products for the current page is less than this number, then we need to just return existing products (we cannot go to the next page)
+    const EXPECTED_NUM_OF_PRODUCTS_PER_PAGE = 24
+    // TODO: add a cron job logic
+    // create a new page
+    const page = await browser.newPage()
+
+    const handle = async () => {
+        try {
+            // connect to the specified url (watch page number)
+            const response = await page.goto(`https://thenewfoodbox.ca/collections/all?filter.v.availability=1&filter.v.price.gte=&filter.v.price.lte=&sort_by=best-selling`, {
+                waitUntil: "domcontentloaded"
+            })
+            // if connection to the page failed (and it is not our API's fault, then send a new request after delay time)
+            if (!response || /^5[0-9][0-9]$/.test(response.status().toString())) {
+                // check if we reached maximum retries, in this case there is no sense to make another request, throw an error
+                if (numOfRetries === constants.MAX_RETRIES) {
+                    // return an error
+                    // TODO: return the cached Product data (firestore)
+                    throw new Error("reached maximum retry count. Consider incrementing the delay time, and sure a request is valid")
+                }
+                // increment the number of retries
+                numOfRetries ++
+                // try to send a new request for the page after some time
+                return await new Promise<Products>(resolve => setTimeout(() => resolve(handle()), 
+                constants.DELAY_BEFORE_NEW_REQUEST))
+            }
+            // if connection to the page failed and this is our API's fault, then throw an error
+            else if (/^4[0-9][0-9]$/.test(response.status().toString())) {
+                // return an error
+                // TODO: return the cached Product data (firestore)
+                throw new Error("check if a request is valid")
+            }
+            // This will create a new function which will be accepting 'productTitle'
+            const getProductCategory = new Function("productTitle", getProductCategory__funcBody)
+            // start function execution
+            return await addProducts()
+            // uncomment for debugging:
+            // let pageNum = 1
+
+            // recursive function we will be calling every time
+            // to go to the next page (there are no products left in the current page)
+            async function addProducts() {
+                try {
+                    // uncomment for debugging:
+                    // console.log("page: " + pageNum)
+                    // pageNum += 1
+                    // wait for the container
+                    await page.waitForSelector("ul[id=product-grid]", { visible: true })
+                    // check if we should add more products to the array: 
+                    if (products.length >= constants.LIMIT_PRODUCT_COUNT) {
+                        // return products
+                        return products
+                    }
+                    // get the current products
+                    const currentProducts = await page.$$("ul[id=product-grid] li.grid__item")
+                    // iterate through each product
+                    for (const currentProduct of currentProducts) {
+                        // get the title 
+                        const title = await currentProduct.evaluate(el => el.querySelector("h3.card__heading a")?.textContent?.trim()) ?? Unavailable.Title
+                        // get the price
+                        const primaryPrice = await currentProduct.evaluate(el => el.querySelector("div.price__regular span.price-item--regular")?.textContent?.trim()) ?? Unavailable.PrimaryPrice
+                        // get units match 
+                        const unitsMatch = title.match(unitsRegex)
+                        // get the units
+                        const units = unitsMatch ? unitsMatch.join(" or ") : Unavailable.Units
+                        // 'getProductCategory' will be returning a product category from one of the enum values from 'ProductCategories' 
+                        const category: ProductCategories = getProductCategory(title)
+                        if (category === ProductCategories.NoCategory) continue
+                        // get a new product
+                        const product: Product = {
+                            imageUri: "my image", 
+                            title, 
+                            primaryPrice, 
+                            units, 
+                            category, 
+                        }
+                        
+                        // if there is a valid product then push it to the array
+                        product && products.push(product)
+                        // uncomment for debugging:
+                        // console.log(`title: ${title}`)
+                    }  
+
+                    // wait for the product container
+                    await page.waitForSelector("ul[id=product-grid]", { visible: true })
+                    // get the number of products for the current page
+                    const productsCount = await page.$eval("ul[id=product-grid]", div => div.querySelectorAll("li.grid__item").length)
+                    // check if this is the last page (then we don't need to wait for the `next` button)
+                    const isLastPage = productsCount < EXPECTED_NUM_OF_PRODUCTS_PER_PAGE
+                    // if this is the last page, then just return existing products
+                    if (isLastPage) return products
+                    // wait for the `next` button
+                    await page.waitForSelector("ul.pagination__list a.pagination__item--prev", { visible: true })
+                    // click on the `next` button and wait for navigation
+                    await Promise.all([
+                        page.click("ul.pagination__list a.pagination__item--prev"), 
+                        // page.waitForNavigation()
+                    ])
+                    // wait for a bit, otherwise, there might be duplicates
+                    // await new Promise(resolve => setTimeout(resolve, 2000))
+                    // call the function again
+                    return await addProducts()
+                }
+                catch (err: any) {
+                    throw new Error("Recursion failed: " + err.message)
+                }  
+            }
+        }
+        catch (err: any) {
+            console.error("Something went wrong (The New Food Box): " + err)
+            // still return products that have been added: 
+            return products
+        }
+        // will be executed regardless
+        finally {
+            // close the browser
+            await browser.close()
+        }
+    }
+
+    // start function execution
+    return await handle()
+}
+
+export default getData
