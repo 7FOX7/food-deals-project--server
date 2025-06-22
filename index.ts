85902587cb1566d85f377d7434d968f7da59a280
7FOX7 <kheeugene@gmail.com>
2025-06-21 22:49:08 -0400

    Added a logic for getting products for Thai Binh Asian Food stores

--- index.ts
+++ index.ts
@@ -33,7 +33,7 @@ const productData: ProductData = {
     "Yogibear's Jellystone Park Camp Resort- Ice Cream/Convenience Store": () => [],    // no data for this store
     "Commisso's Fresh Food": () => import("./scraping/Commisso's Fresh Food/data").then(mod => mod.default()),
     "Farm Boy": () => import("./scraping/Farm Boy/data").then(mod => mod.default()),
-    "Thai Binh Asian Food": () => [],       // no data for this store 
+    "Thai Binh Asian Food": () => import("./scraping/Thai Binh/data").then(mod => mod.default()),
     "The Red Barn Farm Market and Bakery": () => [],  
     "Independent Grocery store - Phil's": () => import("./scraping/Independent Grocery/data").then(mod => mod.default()),
     "Charlie Asian Grocery Inc.": () => [],  // no data for this store 
@@ -119,7 +119,7 @@ function updateProducts() {
 async function testProducts() {
     console.log("will promise to execute!")
     try {
-        const products = await productData["Dinh Dinh Asian Foods"]()
+        const products = await productData["Thai Binh Asian Food"]()
         console.log("products: " + JSON.stringify(products))
         console.log("products length: " + products.length)
     }
---
+++ scraping/Thai Binh/data.ts
@@ -0,0 +1,170 @@
+import puppeteer from "puppeteer"
+import { Products, Product } from "../../utils/types" 
+import { getProductCategory__funcBody, unitsRegex } from "../../utils/regexes"
+import * as constants from "../../utils/constants"
+
+// default for unavailable product data
+enum Unavailable {
+    ImageUri = "[COULD NOT GET PRODUCT IMAGE]", 
+    Title = "[COULD NOT GET PRODUCT NAME]", 
+    PrimaryPrice = "[COULD NOT GET PRODUCT PRIMARY PRICE]", 
+    Units = "[COULD NOT GET PRODUCT UNIT DETAILS]"
+}
+
+// we will be storing food categories: 
+// NOTE: food categories MUST match 'FoodTypes' type from our client app
+enum ProductCategories {
+    FreshProduce = "Fresh Produce",
+    MeatAndSeafood = "Meat & Seafood", 
+    DairyAndEggs = "Dairy & Eggs", 
+    FrozenAndPrepared = "Frozen & Prepared Foods", 
+    BakeryAndBreakfast = "Bakery & Breakfast", 
+    SnacksAndSweets = "Snacks & Sweets", 
+    PantryAndEssentials = "Pantry & Essentials", 
+
+    // this category will be used as a placeholder for the product 
+    // whose food category does not match any of the above (DON'T USE IT IN YOUR client app)
+    NoCategory = "[NO CATEGORY]"
+}
+
+const getData = async (): Promise<Products> => {
+    // will be storing products we will be pushing to the array: 
+    const products: Products = []
+    // will be storing the number of retries we made 
+    // to connect to the page
+    let numOfRetries = 0
+    // will be representing a browser: 
+    const browser = await puppeteer.launch({
+        headless: false
+    })
+    // NOTE: 
+    // - this value will be used to determine if we should look for the `next` button at all
+    // - if the number of products for the current page is less than this number, then we need to just return existing products (we cannot go to the next page)
+    const EXPECTED_NUM_OF_PRODUCTS_PER_PAGE = 24
+    // TODO: add a cron job logic
+    // create a new page
+    const page = await browser.newPage()
+
+    const handle = async () => {
+        try {
+            // connect to the specified url (watch page number)
+            const response = await page.goto(`https://haisue.ca/collections/all-products-and-in-stock`, {
+                waitUntil: "domcontentloaded"
+            })
+            // if connection to the page failed (and it is not our API's fault, then send a new request after delay time)
+            if (!response || /^5[0-9][0-9]$/.test(response.status().toString())) {
+                // check if we reached maximum retries, in this case there is no sense to make another request, throw an error
+                if (numOfRetries === constants.MAX_RETRIES) {
+                    // return an error
+                    // TODO: return the cached Product data (firestore)
+                    throw new Error("reached maximum retry count. Consider incrementing the delay time, and sure a request is valid")
+                }
+                // increment the number of retries
+                numOfRetries ++
+                // try to send a new request for the page after some time
+                return await new Promise<Products>(resolve => setTimeout(() => resolve(handle()), 
+                constants.DELAY_BEFORE_NEW_REQUEST))
+            }
+            // if connection to the page failed and this is our API's fault, then throw an error
+            else if (/^4[0-9][0-9]$/.test(response.status().toString())) {
+                // return an error
+                // TODO: return the cached Product data (firestore)
+                throw new Error("check if a request is valid")
+            }
+            // This will create a new function which will be accepting 'productTitle'
+            const getProductCategory = new Function("productTitle", getProductCategory__funcBody)
+            // start function execution
+            return await addProducts()
+            // uncomment for debugging:
+            // let pageNum = 1
+
+            // recursive function we will be calling every time
+            // to go to the next page (there are no products left in the current page)
+            async function addProducts() {
+                try {
+                    // uncomment for debugging:
+                    // console.log("page: " + pageNum)
+                    // pageNum += 1
+                    // wait for the container
+                    await page.waitForSelector("div.main-products-grid__results", { visible: true })
+                    // check if we should add more products to the array: 
+                    if (products.length >= constants.LIMIT_PRODUCT_COUNT) {
+                        // return products
+                        return products
+                    }
+                    // get the current products
+                    const currentProducts = await page.$$("div.main-products-grid__results ul li.js-pagination-result")
+                    // iterate through each product
+                    for (const currentProduct of currentProducts) {
+                        // get the title 
+                        const title = await currentProduct.evaluate(el => el.querySelector("p.card__title a")?.textContent?.trim()) ?? Unavailable.Title
+                        // get price part 1
+                        const pricePart1 = await currentProduct.evaluate(el => el.querySelector("div.price__default span")?.firstChild?.textContent?.trim())
+                        // get price part 2 
+                        const pricePart2 = await currentProduct.evaluate(el => el.querySelector("div.price__default span sup")?.textContent?.trim())
+                        // build the price
+                        const primaryPrice = pricePart1 && pricePart2 ? `${pricePart1}.${pricePart2}` : Unavailable.PrimaryPrice
+                        // get units match 
+                        const unitsMatch = title.match(unitsRegex)
+                        // get the units
+                        const units = unitsMatch ? unitsMatch.join(" or ").toLowerCase() : Unavailable.Units
+                        // 'getProductCategory' will be returning a product category from one of the enum values from 'ProductCategories' 
+                        const category: ProductCategories = getProductCategory(title)
+                        if (category === ProductCategories.NoCategory) continue
+                        // get a new product
+                        const product: Product = {
+                            imageUri: "my image", 
+                            title, 
+                            primaryPrice, 
+                            units, 
+                            category, 
+                        }
+                        
+                        // if there is a valid product then push it to the array
+                        product && products.push(product)
+                        // uncomment for debugging:
+                        // console.log(`title: ${title}`)
+                    }  
+
+                    // wait for the product container
+                    await page.waitForSelector("div.main-products-grid__results ul", { visible: true })
+                    // get the number of products for the current page
+                    const productsCount = await page.$eval("div.main-products-grid__results ul", div => div.querySelectorAll("li.js-pagination-result").length)
+                    // check if this is the last page (then we don't need to wait for the `next` button)
+                    const isLastPage = productsCount < EXPECTED_NUM_OF_PRODUCTS_PER_PAGE
+                    // if this is the last page, then just return existing products
+                    if (isLastPage) return products
+                    // wait for the `next` button
+                    await page.waitForSelector("a.pagination__arrow--next", { visible: true })
+                    // click on the `next` button and wait for navigation
+                    await Promise.all([
+                        page.click("a.pagination__arrow--next"), 
+                        // page.waitForNavigation()         this won't work
+                    ])
+                    // wait for a bit to make sure we navigated to the next page
+                    await new Promise(resolve => setTimeout(resolve, 2000))
+                    // call the function again
+                    return await addProducts()
+                }
+                catch (err: any) {
+                    throw new Error("Recursion failed: " + err.message)
+                }  
+            }
+        }
+        catch (err: any) {
+            console.error("Something went wrong (Thai Binh Asian Food): " + err)
+            // still return products that have been added: 
+            return products
+        }
+        // will be executed regardless
+        finally {
+            // close the browser
+            await browser.close()
+        }
+    }
+
+    // start function execution
+    return await handle()
+}
+
+export default getData
